58ae0a5c543ecc5f2fb7020749835517ba2f4bdd
ael-code <tommy.ael@gmail.com>
2015-08-20 12:19:37 +0200

    improove simple_data_getter

--- authbone/auth_data_getters.py
+++ authbone/auth_data_getters.py
@@ -1,8 +1,19 @@
 from flask import request
-
+from authentication import AuthDataDecodingException
 
 def simple_data_getter():
     auth_data = dict()
-    auth_data['username'] = request.args.get('username', None)
-    auth_data['password'] = request.args.get('password', None)
+    try:
+        auth_data['username'] = request.args['username']
+    except KeyError:
+        raise AuthDataDecodingException('bad credentials: missing "username"')
+
+    try:
+        auth_data['password'] = request.args['password']
+    except KeyError:
+        raise AuthDataDecodingException('bad credentials: missing "password"')
+
     return auth_data
+
+def basic_auth_getter():
+    return request.authorization
