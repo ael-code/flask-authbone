66d075277c374af3ee347552e4271c6d266bad3f
ael-code <tommy.ael@gmail.com>
2015-09-13 17:33:27 +0200

    tests: added authroization tests

--- test/test_authorization.py
+++ test/test_authorization.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import Flask
-from authbone import Authorizator, Authenticator
+from authbone import Authorizator, Authenticator, CapabilityMissingException
 from authbone.auth_data_getters import form_data_getter
 
 
@@ -28,7 +28,7 @@ app = Flask(__name__)
 app.config['TESTING'] = True
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 @authorizator.requires_capability(None)
 def root():
     return 'ok'
@@ -42,6 +42,15 @@ class FlaskTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_authorized(self):
+        rs = self.tc.post('/', data={'username': 'admin',
+                                     'password': 'admin'})
+        assert(rs.status_code == 200)
+
+    def test_capability_missing(self):
+        with self.assertRaises(CapabilityMissingException):
+            self.tc.post('/', data={'username': 'test',
+                                    'password': 'test'})
 
 if __name__ == '__main__':
     unittest.main()
