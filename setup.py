9ebff25ad07ee7a1fe35d6f740e36a1a661556b5
ael-code <tommy.ael@gmail.com>
2016-06-21 13:36:55 +0200

    bump to v0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Authbone',
-    version='0.2.1',
+    version='0.2.2',
     url='https://github.com/ael-code/flask-authbone',
     license='MIT',
     author='ael',
